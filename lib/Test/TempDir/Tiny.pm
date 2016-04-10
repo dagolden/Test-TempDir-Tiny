d4866f2918d781397c59b04daab0d2e98201160a
David Golden <xdg@xdg.me>
2016-04-10 13:47:52 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/Test/TempDir/Tiny.pm
+++ lib/Test/TempDir/Tiny.pm
@@ -5,7 +5,7 @@ use warnings;
 package Test::TempDir::Tiny;
 # ABSTRACT: Temporary directories that stick around when tests fail
 
-our $VERSION = '0.016';
+our $VERSION = '0.017';
 
 use Exporter 5.57 qw/import/;
 our @EXPORT = qw/tempdir in_tempdir/;
